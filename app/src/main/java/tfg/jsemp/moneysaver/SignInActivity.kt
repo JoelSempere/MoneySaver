73e1f489d54d57816dc229f7ee145d176b00506e
Joel Sempere <joelsempere96@gmail.com>
2022-04-17 20:04:15 +0200

    Login con Kotlin personalizado, pestaña de login y registrarse. V1 del Acceso

--- app/src/main/java/tfg/jsemp/moneysaver/SignInActivity.kt
+++ app/src/main/java/tfg/jsemp/moneysaver/SignInActivity.kt
@@ -1,19 +1,17 @@
 package tfg.jsemp.moneysaver
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_in.*
 import tfg.jsemp.moneysaver.utils.ConstantsUtil.constantesLogin.LOGIN_EMAIL
 
-
-class LoginActivity : AppCompatActivity() {
-
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_in)
         iniciarApp()
     }
 
@@ -34,34 +32,31 @@ class LoginActivity : AppCompatActivity() {
     }
 
 
-    private fun createSingInIntent() {
-        val loginIntent = Intent(this, SignInActivity::class.java)
-        startActivity(loginIntent)
-    }
-
-
     //***COMPRUEBA CONTENIDO EN LOS EDIT TEXT***//
     private fun comprobarCamposLogin(): Boolean {
-     return (etEmail.text.isNotEmpty() && etPassword.text.isNotEmpty())
+        return (
+                etEmail.text.isNotEmpty() && etPassword.text.isNotEmpty() && etPassword2.text.isNotEmpty() &&
+                        (
+                                etPassword.text.toString() == etPassword2.text.toString()
+                                )
+                )
     }
 
 
-    //***ACCIONES DE LOS CLICKS PARA INICIAR APP***//
+    //***ACCION DE REGISTRO PARA INCIAR APP***//
     private fun iniciarApp() {
         btnSignIn.setOnClickListener {
-            createSingInIntent()
-        }
-        btnLogin.setOnClickListener {
+            println("@Joel: Comprobar Boolean - 17/04/22 - " + comprobarCamposLogin())
             if (comprobarCamposLogin()) {
                 FirebaseAuth.getInstance()
-                    .signInWithEmailAndPassword(
+                    .createUserWithEmailAndPassword(
                         etEmail.text.toString(),
                         etPassword.text.toString()
                     )
-                    .addOnCompleteListener{
-                        if (!it.isSuccessful){
+                    .addOnCompleteListener {
+                        if (!it.isSuccessful) {
                             getErrorSignIn()
-                        }else{
+                        } else {
                             createLoginIntent()
                         }
                     }
---
+++ app/src/main/java/tfg/jsemp/moneysaver/utils/ConstantsUtil.kt
@@ -0,0 +1,8 @@
+package tfg.jsemp.moneysaver.utils
+
+class ConstantsUtil {
+
+    object constantesLogin{
+        const val LOGIN_EMAIL : String = "tfg.jsemp.moneysaver.utils.LOGIN_EMAIL"
+    }
+}
